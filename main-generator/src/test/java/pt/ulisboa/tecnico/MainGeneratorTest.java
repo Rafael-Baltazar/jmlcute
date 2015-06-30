8eb1008fbdd652f7b332f48582e3a94ccb501f01
Rafael Baltazar <rafael.baltazar@live.com.pt>
2015-06-30 02:16:24 +0100

    Completed test case.

--- main-generator/src/test/java/pt/ulisboa/tecnico/MainGeneratorTest.java
+++ main-generator/src/test/java/pt/ulisboa/tecnico/MainGeneratorTest.java
@@ -25,9 +25,30 @@ public class MainGeneratorTest extends TestCase {
     }
 
     /**
-     * Rigourous Test :-)
+     * Test App.
      */
     public void testApp() {
-        assertTrue(true);
+        MainGenerator mainGenerator = new MainGenerator();
+        StringBuilder[] stringBuilders = new StringBuilder[0];
+        try {
+            stringBuilders = mainGenerator.generate("App.App");
+        } catch (ClassNotFoundException e) {
+            fail("App.App was not found.");
+        }
+        assertTrue("stringBuilders is of incorrect size.", stringBuilders
+                .length == 1);
+        StringBuilder sb = new StringBuilder();
+        sb.append("package App;\n");
+        sb.append("\n");
+        sb.append("public class jmlcute__App__App__app_App_App {\n");
+        sb.append("  public static void main(String[] args) {\n");
+        sb.append("    App.App receiver = cute.Cute.input.Object(\"\");\n");
+        sb.append("    cute.Cute.Assume(receiver != null);\n");
+        sb.append("    App.App arg0 = cute.Cute.input.Object(\"App.App\");\n");
+        sb.append("    App.App arg1 = cute.Cute.input.Object(\"App.App\");\n");
+        sb.append("    receiver.app(arg0, arg1);\n");
+        sb.append("  }\n");
+        sb.append("}\n");
+        assertTrue("stringBuilders[0] is incorrect.", sb.toString().equals(stringBuilders[0].toString()));
     }
 }
